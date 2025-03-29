1427877670631a3f4b5af296e9e0b860080f6353
rainool <ryanliu05@gmail.com>
2025-03-29 22:46:13 +1100

    This is the source file for the program

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+string userInput;
+const string fontPath = @"C:\Users\ryanl\anaconda3\Lib\site-packages\panel\dist\bundled\reveal.js@4.5.0\dist\theme\fonts\league-gothic\league-gothic.ttf";
+
+//Ask for text
+WriteLine("Enter your text:");
+userInput = ReadLine();
+
+//Create a window to display user input
+OpenWindow("Programming Experiement Ryan Liu S224476007",800,600);
+ClearScreen(ColorWhite());
+
+Font font1 = LoadFont("LeagueGothic", fontPath);
+
+//Calculate dimensions of text
+int textWidth = TextWidth(userInput, font1, 24);
+int textHeight = TextHeight(userInput, font1, 24);
+
+//Calculate the centre position for any given size text
+int x = (800 - textWidth) / 2;
+int y = (600 - textHeight) / 2;
+
+//Write text into window
+DrawText(userInput, ColorBlack(), x, y);
+
+RefreshScreen();
+Delay(20*1000);
+
+//"C:\Users\ryanl\anaconda3\Lib\site-packages\panel\dist\bundled\reveal.js@4.5.0\dist\theme\fonts"
